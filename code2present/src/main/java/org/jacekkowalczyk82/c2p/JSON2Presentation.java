2c1bee0619258339d1ae50f6a6ab6f2ca40c821e
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2024-06-12 21:51:06 +0200

    triong PDF export of pptx

--- code2present/src/main/java/org/jacekkowalczyk82/c2p/JSON2Presentation.java
+++ code2present/src/main/java/org/jacekkowalczyk82/c2p/JSON2Presentation.java
@@ -1,10 +1,23 @@
 package org.jacekkowalczyk82.c2p;
 
 
+import com.lowagie.text.*;
+import com.lowagie.text.Image;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfGraphics2D;
+import com.lowagie.text.pdf.PdfWriter;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFBackground;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.jacekkowalczyk82.c2p.model.Presentation;
 import org.jacekkowalczyk82.c2p.model.Slide;
 
-import java.io.IOException;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -46,6 +59,96 @@ public class JSON2Presentation {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
+            //save to PDF
+
+//            try {
+//                FileInputStream iStream = new FileInputStream(new File(pres.getConfiguration().getTargetPresentationFile()));
+//
+//                XMLSlideShow presentation = new XMLSlideShow(iStream);
+//                Dimension pgSize  = presentation.getPageSize();
+                Dimension pgSize  = c2p.getPresentation().getPageSize();
+                Document pdfDoc = new Document();
+                double zoom = 1; // magnify it by 2 as typical slides are low res
+                AffineTransform at = new AffineTransform();
+                at.setToScale(zoom, zoom);
+
+                try {
+                    PdfWriter pdfWriter = PdfWriter.getInstance(pdfDoc,
+                            new FileOutputStream(pres.getConfiguration().getTargetPresentationFile()+ ".pdf"));
+                    pdfDoc.setPageSize(new Rectangle((float)pgSize.getWidth(), (float)pgSize.getHeight()));
+                    pdfDoc.open();
+//                    pdfDoc.add(new Chunk(""));
+
+                    LOGGER.debug("Opening PDF Document");
+                    for (XSLFSlide slide: c2p.getPresentation().getSlides()) {
+                        LOGGER.debug("Adding slide "+ slide.getTitle());
+
+                        BufferedImage bufImg = new BufferedImage((int)Math.ceil(pgSize.width*zoom),
+                                (int)Math.ceil(pgSize.height*zoom), BufferedImage.TYPE_INT_RGB);
+                        Graphics2D graphics = bufImg.createGraphics();
+                        graphics.setTransform(at);
+                        //clear the drawing area
+                        XSLFBackground bcGround = slide.getBackground();
+                        Color bgColor = null;
+                        if (bcGround != null) {
+                            bgColor = slide.getBackground().getFillColor();
+
+                        }
+                        if (bgColor!=null) {
+                            graphics.setPaint(bgColor);
+                        }
+                        else {
+                            graphics.setPaint(Color.white);
+                        }
+
+
+                        graphics.fill(new Rectangle2D.Float(0, 0, pgSize.width, pgSize.height));
+                        try{
+                            slide.draw(graphics);
+
+                        } catch(Exception e){
+                            //Just ignore, draw what I have
+                            e.printStackTrace();
+                        }
+
+                        Image image = null;
+                        try {
+                            image = Image.getInstance(bufImg, null);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        pdfDoc.setPageSize(new Rectangle(image.getScaledWidth(), image.getScaledHeight()));
+                        LOGGER.debug("At page "+ pdfDoc.getPageNumber());
+
+//                        image.setAbsolutePosition(0, 0);
+                        pdfDoc.add(image);
+
+                        pdfDoc.newPage();
+//
+//                    PdfGraphics2D graphics  = (PdfGraphics2D) pdfWriter.getDirectContent().createGraphics((float)pgSize.getHeight(),
+//                            (float) pgSize.getHeight());
+//                    slide.draw(graphics);
+//                    graphics.dispose();
+                    }
+
+                    pdfDoc.close();
+
+                } catch (DocumentException e) {
+                    throw new RuntimeException(e);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+//
+//            } catch (FileNotFoundException e) {
+//                throw new RuntimeException(e);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+
+
         }
 
     }
